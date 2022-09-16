6b39a3f0dcf36ea28bf18bb49b4d07fa3da628e5
Matheus Rovero Rodrigues <vector.320@outlook.com>
2022-09-15 21:20:50 -0300

    Criando a View "Details" e a pasta ViewModels

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
@@ -25,9 +26,19 @@ public class HomeController : Controller
         return View(pokemons);
     }
 
-    public IActionResult Details()
+    public IActionResult Details(uint Number)
     {
-        return View();
+        var current = _context.Pokemons.Include(p => p.Types).ThenInclude (pt => pt.Type).Include(p => p.Gender).Include(p => p.Generation).Where (p => p.Number == Number).SingleOrDefault();
+        var prior = _context.Pokemons.OrderByDescending(p => p.Number).Where(p => p.Number < Number).FirstOrDefault();
+        var next = _context.Pokemons.OrderBy(p => p.Number).Where(p => p.Number > Number).FirstOrDefault();
+
+        var pokemon = new Details()
+        {
+            Prior = prior,
+            Current = current,
+            Next = next,
+        };
+        return View(pokemon);
     }
 
     public IActionResult Privacy()
---
+++ ViewModels/Details.cs
@@ -0,0 +1,12 @@
+using Pokedex.Models;
+
+
+namespace Pokedex.ViewModels
+{
+    public class Details
+    {
+        public Pokemons? Prior { get; set; }
+        public Pokemons? Current { get; set; }
+        public Pokemons? Next { get; set; }
+    }
+}
